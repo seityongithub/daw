84afad03722cfce97d9231c25881076eb5f6f2dd
Seity <107432634+seityongithub@users.noreply.github.com>
2026-03-22 03:09:18 +0800

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Link from "next/link"
+import Image from "next/image"
 import { usePathname } from "next/navigation"
 import { useState, useEffect } from "react"
 import { cn } from "@/lib/utils"
